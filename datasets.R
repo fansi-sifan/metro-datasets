ad3ee2ed44265221af0d53a6e627857a384ef9a7
Sifan Liu <SLiu@brookings.edu>
2019-06-18 10:17:34 +1100

    apply styler::tidyverse_style()

--- datasets.R
+++ datasets.R
@@ -2,13 +2,13 @@
 # Author: Sifan Liu
 # Date: Fri Aug 03 14:00:12 2018
 # SET UP ==============================================
-pkgs <- c('tidyverse', 'reshape2','writexl', 'httr')
+pkgs <- c("tidyverse", "reshape2", "writexl", "httr")
 
-check <- sapply(pkgs,require,warn.conflicts = TRUE,character.only = TRUE)
-if(any(!check)){
+check <- sapply(pkgs, require, warn.conflicts = TRUE, character.only = TRUE)
+if (any(!check)) {
   pkgs.missing <- pkgs[!check]
   install.packages(pkgs.missing)
-  check <- sapply(pkgs.missing,require,warn.conflicts = TRUE,character.only = TRUE)
+  check <- sapply(pkgs.missing, require, warn.conflicts = TRUE, character.only = TRUE)
 }
 
 # TRANSFORM ============================================
@@ -22,15 +22,15 @@ prosperity_change <- read.csv(paste0(paths, "Prosperity/Prosperity Ranks (IS 201
 inclusion_change <- read.csv(paste0(paths, "Inclusion/Inclusion Ranks (IS 2017.11.17).csv"))
 
 # absolute value in 2016
-growth_value <- read.csv(paste0(paths,"Growth/Growth Values (IS 2017.11.15).csv")) %>%
+growth_value <- read.csv(paste0(paths, "Growth/Growth Values (IS 2017.11.15).csv")) %>%
   filter(year == 2016) %>%
   dcast(year + cbsa ~ indicator, var.value = "value")
 
-prosperity_value <- read.csv(paste0(paths,"Prosperity/Prosperity Values (IS 2017.11.14).csv")) %>%
+prosperity_value <- read.csv(paste0(paths, "Prosperity/Prosperity Values (IS 2017.11.14).csv")) %>%
   filter(year == 2016) %>%
   dcast(year + CBSA ~ indicator, var.value = "value")
 
-inclusion_value <- read.csv(paste0(paths,"Inclusion/Inclusion Values (IS 2017.11.17).csv")) %>%
+inclusion_value <- read.csv(paste0(paths, "Inclusion/Inclusion Values (IS 2017.11.17).csv")) %>%
   filter(year == 2016) %>%
   filter(race == "Total") %>%
   filter(eduatt == "Total") %>%
@@ -65,24 +65,24 @@ cbsa_metromonitor <- cbsa_change %>%
 
 # Export Monitor---------------------------------------------------
 cbsa_export <- readxl::read_xlsx("V:/Export Monitor/2018/Deliverables/Deliverables/Metros Data/Metros by Total, NAICS 2 3.xlsx", sheet = "Total") %>%
-  filter(Year == 2017)  %>%
+  filter(Year == 2017) %>%
   mutate(cbsa = as.character(`(CBSA)`))
 
 county_export <- readxl::read_xlsx("V:/Export Monitor/2018/Deliverables/Deliverables/Counties Data/Counties by Total, NAICS 2.xlsx", sheet = "Total") %>%
   filter(Year == 2017) %>%
-  mutate(FIPS = str_pad(as.character(`(County)`), 5,"left","0"))
+  mutate(FIPS = str_pad(as.character(`(County)`), 5, "left", "0"))
 
 cbsa_naics4_export <- read.csv("V:/Export Monitor/2018/Deliverables/Deliverables/Metros Data/Metros  by NAICS 4.csv") %>%
   filter(gm == msa_FIPS) %>%
   filter(year == 2017)
 
-county_naics4_export <- read.csv("V:/Export Monitor/2018/Deliverables/Deliverables/Counties Data/Counties by NAICS 4.csv")%>%
-  filter(gc == as.integer(county_FIPS))%>%
+county_naics4_export <- read.csv("V:/Export Monitor/2018/Deliverables/Deliverables/Counties Data/Counties by NAICS 4.csv") %>%
+  filter(gc == as.integer(county_FIPS)) %>%
   filter(year == 2017)
 
 
 # ShiftShare ---------------------------------------------------
-cbsa_shiftshare <- read.csv('V:/Performance/Project files/Metro Monitor/2018v/Output/Shift Share/Monitor Shiftshare Cumulative (2-digit NAICS).csv') %>%
+cbsa_shiftshare <- read.csv("V:/Performance/Project files/Metro Monitor/2018v/Output/Shift Share/Monitor Shiftshare Cumulative (2-digit NAICS).csv") %>%
   filter(cbsa2013_fips == msa_FIPS) %>%
   filter(year == 2016)
 
@@ -91,36 +91,42 @@ cbsa_digital <- read.csv("V:/Sifan/Birmingham/County Cluster/source/metro_all_up
   mutate(cbsa = as.character(`AREA`))
 
 # Univ R&D ---------------------------------------------------
-NSF_univRD <- read.csv('V:/Sifan/Birmingham/County Cluster/source/NSF_univ.csv')
+NSF_univRD <- read.csv("V:/Sifan/Birmingham/County Cluster/source/NSF_univ.csv")
 
 cbsa_univRD <- NSF_univRD %>%
   group_by(cbsacode) %>%
-  summarise(RDtotal = sum(Deflated.Total.R.D.Expenditures.in.All.Fields.Sum.),
-            RDtotal_biz = sum(as.numeric(as.character(Deflated.Business.Financed.R.D.Expenditures.Sum.)))) %>%
+  summarise(
+    RDtotal = sum(Deflated.Total.R.D.Expenditures.in.All.Fields.Sum.),
+    RDtotal_biz = sum(as.numeric(as.character(Deflated.Business.Financed.R.D.Expenditures.Sum.)))
+  ) %>%
   mutate(cbsa = as.character(cbsacode))
 
 county_univRD <- NSF_univRD %>%
   group_by(county) %>%
-  summarise(RDtotal = sum(Deflated.Total.R.D.Expenditures.in.All.Fields.Sum.),
-            RDtotal_biz = sum(as.numeric(as.character(Deflated.Business.Financed.R.D.Expenditures.Sum.)))) %>%
-  mutate(FIPS = str_pad(as.character(county), 5,"left","0"))
+  summarise(
+    RDtotal = sum(Deflated.Total.R.D.Expenditures.in.All.Fields.Sum.),
+    RDtotal_biz = sum(as.numeric(as.character(Deflated.Business.Financed.R.D.Expenditures.Sum.)))
+  ) %>%
+  mutate(FIPS = str_pad(as.character(county), 5, "left", "0"))
 
 
 # AUTM ---------------------------------------------------
 AUTM <- read.csv("V:/Sifan/Birmingham/County Cluster/source/AUTM.csv")
 
 county_AUTM <- AUTM %>%
-  group_by(FIPS)%>%
+  group_by(FIPS) %>%
   # summarise_if(is.numeric, sum, na.rm = TRUE) %>%
-  summarise(tot_lic = sum(Lic.Iss,na.rm = TRUE)+sum(Opt.Iss,na.rm = TRUE),
-            lg_lic = sum(Tot.Lic.Lg.Co,na.rm = TRUE),
-            sm_lic = sum(Tot.Lic.Sm.Co,na.rm = TRUE),
-            st_lic = sum(Tot.Lic.St.Ups,na.rm = TRUE),
-            inc_lic = sum(Gross.Lic.Inc,na.rm = TRUE),
-            tot_IP = sum(Inv.Dis.Rec,na.rm = TRUE),
-            tot_st = sum(St.Ups.Formed,na.rm = TRUE),
-            instate_st = sum(St.Ups.in.Home.St,na.rm = TRUE))%>%
-  mutate(FIPS = str_pad(as.character(FIPS), 5,"left","0"))
+  summarise(
+    tot_lic = sum(Lic.Iss, na.rm = TRUE) + sum(Opt.Iss, na.rm = TRUE),
+    lg_lic = sum(Tot.Lic.Lg.Co, na.rm = TRUE),
+    sm_lic = sum(Tot.Lic.Sm.Co, na.rm = TRUE),
+    st_lic = sum(Tot.Lic.St.Ups, na.rm = TRUE),
+    inc_lic = sum(Gross.Lic.Inc, na.rm = TRUE),
+    tot_IP = sum(Inv.Dis.Rec, na.rm = TRUE),
+    tot_st = sum(St.Ups.Formed, na.rm = TRUE),
+    instate_st = sum(St.Ups.in.Home.St, na.rm = TRUE)
+  ) %>%
+  mutate(FIPS = str_pad(as.character(FIPS), 5, "left", "0"))
 
 
 # REGPAT ---------------------------------------------------
@@ -128,31 +134,31 @@ cbsa_REGPAT <- readxl::read_xlsx("V:/Global Profiles/Data/REGPAT/Analysis Files/
   filter(`Year Range` == "2008-2012")
 
 # USPTO ---------------------------------------------------
-cbsa_USPTO <- read.csv('V:/Sifan/Birmingham/County Cluster/source/USPTO_msa.csv') %>%
-  mutate(cbsa = substr(as.character(ID.Code),2,6))
+cbsa_USPTO <- read.csv("V:/Sifan/Birmingham/County Cluster/source/USPTO_msa.csv") %>%
+  mutate(cbsa = substr(as.character(ID.Code), 2, 6))
 
-county_USPTO <- read.csv('V:/Sifan/Birmingham/County Cluster/source/USPTO_county.csv') %>%
-  mutate(FIPS = str_pad(as.character(FIPS.Code), 5,"left","0"))
+county_USPTO <- read.csv("V:/Sifan/Birmingham/County Cluster/source/USPTO_county.csv") %>%
+  mutate(FIPS = str_pad(as.character(FIPS.Code), 5, "left", "0"))
 
 # Patent Complexity ---------------------------------------------------
-cbsa_patentCOMP <- read.csv('V:/Sifan/Birmingham/County Cluster/source/Complexity_msa.csv') %>%
+cbsa_patentCOMP <- read.csv("V:/Sifan/Birmingham/County Cluster/source/Complexity_msa.csv") %>%
   mutate(cbsa = as.character(cbsa))
 
 # VC ---------------------------------------------------
-cbsa_VC <- read.csv('V:/Sifan/Birmingham/County Cluster/source/VC.csv') %>%
+cbsa_VC <- read.csv("V:/Sifan/Birmingham/County Cluster/source/VC.csv") %>%
   filter(round == "Total VC" & measure == "Capital Invested ($ M) per 1M Residents") %>%
   mutate(cbsa = as.character(cbsa13))
 
 # Inc5000 ---------------------------------------------------
-cbsa_I5HGC <- read.csv("V:/Sifan/Birmingham/County Cluster/source/I5HGC_density.csv")%>%
+cbsa_I5HGC <- read.csv("V:/Sifan/Birmingham/County Cluster/source/I5HGC_density.csv") %>%
   mutate(cbsa = as.character(CBSA))
 
 # Broadband ---------------------------------------------------
 tract_broadband <- readxl::read_xlsx("V:/Infrastructure/2 Long Form Projects/Broadband/Final Layout/Masterfile_Final.xlsx")
 
 # out of work ---------------------------------------------------
-county_OoW <- read.csv('V:/Sifan/Birmingham/County Cluster/source/OutOfWork_county.csv') %>%
-  mutate(FIPS = str_pad(fips, 5,"left","0"))
+county_OoW <- read.csv("V:/Sifan/Birmingham/County Cluster/source/OutOfWork_county.csv") %>%
+  mutate(FIPS = str_pad(fips, 5, "left", "0"))
 
 
 # SAVE OUTPUT ==============================================
