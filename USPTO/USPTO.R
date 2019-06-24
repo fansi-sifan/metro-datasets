a4ba9b5a6fcc7337d8b1dcef76fa91a0e3a6899e
eleanorble <enoble1@macalester.edu>
2019-06-24 15:56:31 -0400

    changed USPTO scripts and data format:  - changed variable classes and names  - wide to long data for both sets  - cleaned year column  - updated metadata and wrote csvs again

--- USPTO/USPTO.R
+++ USPTO/USPTO.R
@@ -2,7 +2,7 @@
 # Author: Eleanor Noble
 # Date: 6/19/2019
 # SET UP ==============================================
-pkgs <- c("tidyverse", "reshape2", "writexl", "httr","skimr", "janitor")
+pkgs <- c("tidyverse", "reshape2", "writexl", "httr","skimr", "janitor", "stringr")
 
 check <- sapply(pkgs, require, warn.conflicts = TRUE, character.only = TRUE)
 if (any(!check)) {
@@ -12,18 +12,46 @@ if (any(!check)) {
 }
 
 # TRANSFORM ============================================
-# USPTO ---------------------------------------------------
+# USPTO CBSA---------------------------------------------------
 cbsa_USPTO <- read.csv("V:/Sifan/Birmingham/County Cluster/source/USPTO_msa.csv") %>% janitor::clean_names() %>%
-  mutate(cbsa = substr(as.character(id_code), 2, 6)) %>%
+  mutate(cbsa_code = substr(as.character(id_code), 2, 6)) %>%
   filter(id_code != "") %>%
   filter(geo_type != "ALL AREAS")
 
+#changing cbsa uspto names and classes
+names(cbsa_USPTO)[names(cbsa_USPTO) == 'geo_type'] <- 'cbsa_type'
+names(cbsa_USPTO)[names(cbsa_USPTO) == 'us_regional_title'] <- 'cbsa_name'
+cbsa_USPTO$id_code<-NULL
+cbsa_USPTO$total<-NULL
+
+cbsa_USPTO$cbsa_type<-as.character(cbsa_USPTO$cbsa_type)
+cbsa_USPTO$cbsa_name<-as.character(cbsa_USPTO$cbsa_name)
+
+#gathering into long instead of wide data
+cbsa_USPTO<-gather(cbsa_USPTO,"year","patents_issued",3:18)
+cbsa_USPTO<-cbsa_USPTO%>%
+  mutate(year=as.numeric(substring(year, 2)))
+
+# USPTO County---------------------------------------------------
 county_USPTO <- read.csv("V:/Sifan/Birmingham/County Cluster/source/USPTO_county.csv") %>% janitor::clean_names() %>%
-  mutate(FIPS = str_pad(as.character(fips_code), 5, "left", "0"))%>%
+  mutate(stco_code = str_pad(as.character(fips_code), 5, "left", "0"))%>%
   filter(mail_code!="ALL")
 
+#changing county uspto names and classes and deleting unnecessary variables
 county_USPTO$fips_code <- NULL
-  
+county_USPTO$mail_code <- NULL
+county_USPTO$total <- NULL
+names(county_USPTO)[names(county_USPTO) == 'state_or_territory'] <- 'st_name'
+names(county_USPTO)[names(county_USPTO) == 'regional_area_component'] <- 'co_name'
+
+county_USPTO$st_name<-as.character(county_USPTO$st_name)
+county_USPTO$co_name<-as.character(county_USPTO$co_name)
+
+#gathering into long instead of wide data
+county_USPTO<-gather(county_USPTO,"year","patents_issued",3:18)
+county_USPTO<-county_USPTO%>%
+  mutate(year=as.numeric(substring(year, 2)))
+
 # check output
 skim_with_defaults()
 skim(cbsa_USPTO)
