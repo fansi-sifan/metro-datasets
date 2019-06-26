f8dbae7bf6365ad3b350fd216785b25d4329fb02
Sifan Liu <sifan1121@gmail.com>
2019-06-26 14:58:48 -0400

    Revert "Metro datasets eleanor"

--- VC/vc.R
+++ VC/vc.R
@@ -2,7 +2,7 @@
 # Author: Eleanor Noble
 # Date: 6/19/2019
 # SET UP ==============================================
-pkgs <- c("tidyverse", "reshape2", "writexl", "httr","skimr", "janitor", "sjlabelled")
+pkgs <- c("tidyverse", "reshape2", "writexl", "httr","skimr", "janitor")
 
 check <- sapply(pkgs, require, warn.conflicts = TRUE, character.only = TRUE)
 if (any(!check)) {
@@ -12,57 +12,44 @@ if (any(!check)) {
 }
 
 # TRANSFORM ============================================
-# vc ---------------------------------------------------
-cbsa_vc <- read.csv("V:/Sifan/Birmingham/County Cluster/source/VC.csv") %>%
+# VC ---------------------------------------------------
+cbsa_VC <- read.csv("V:/Sifan/Birmingham/County Cluster/source/VC.csv") %>%
   filter(round == "Total VC" & measure == "Capital Invested ($ M) per 1M Residents") %>%
   mutate(cbsa_code = as.character(cbsa13)) %>%
   janitor::clean_names()
 
 # changing variable names, classes, getting rid of unnecessary variables
-cbsa_vc$x <- NULL
-cbsa_vc$cbsa13 <-NULL
-cbsa_vc$country<-NULL
-cbsa_vc$measure <- NULL
+cbsa_VC$x <- NULL
+cbsa_VC$cbsa13 <-NULL
+cbsa_VC$country<-NULL
+cbsa_VC$measure <- NULL
 
-colnames(cbsa_vc)[which(names(cbsa_vc) == "msa")] <- "cbsa_name"
+colnames(cbsa_VC)[which(names(cbsa_VC) == "msa")] <- "cbsa_name"
+colnames(cbsa_VC)[which(names(cbsa_VC) == "value")] <- "capital_invested_(in_millions)_per_1M_residents"
 
-cbsa_vc$cbsa_name<-as.character(cbsa_vc$cbsa_name)
-cbsa_vc$rank<-as.numeric(cbsa_vc$rank)
-
-#labels for metadata
-labels<-c("cbsa name","time period of investment","total venture capital","
-capital invested (in_millions) per 1M residents","latitude","longitude","cbsa ranking ","cbsa code")
-
-set_label(cbsa_vc)<-labels
-
-#correspondance between labels and variable names
-cbsa_vc_key <- get_label(cbsa_vc) %>%
-  data.frame() %>%
-  rename_at(vars(1), funs(paste0('labels'))) %>%
-  mutate(names = colnames(cbsa_vc))
+cbsa_VC$cbsa_name<-as.character(cbsa_VC$cbsa_name)
 
 # check output
 skim_with_defaults()
-skim(cbsa_vc)
+skim(cbsa_VC)
 
 # save output
-dir.create("vc")
+dir.create("VC")
 
-save(cbsa_vc,file = "vc/cbsa_vc.rda")
+save(cbsa_VC,file = "VC/cbsa_VC.rda")
 
 # generate metadata county
-sink("vc/cbsa_vc.txt")
-cbsa_vc_key
+sink("VC/cbsa_VC.txt")
+
 skim_with(numeric = list(hist = NULL))
-skim(cbsa_vc)
+skim(cbsa_VC)
 sink()
 
 # create README cbsa
-sink("vc/README.md")
-kable(cbsa_vc_key)
-skim(cbsa_vc)%>% kable()
+sink("VC/cbsa_VC.md")
+skim(cbsa_VC)%>% kable()
 sink()
 
 # write csv to github
-write.csv(cbsa_vc, "vc/cbsa_vc.csv")
+write.csv(cbsa_VC, "VC/cbsa_VC.csv")
 
--- out_of_work/out_of_work.R
+++ out_of_work/out_of_work.R
@@ -2,7 +2,7 @@
 # Author: Eleanor Noble
 # Date: 6/19/2019
 # SET UP ==============================================
-pkgs <- c("tidyverse", "reshape2", "writexl", "httr","skimr", "janitor", "stringr","sjlabelled")
+pkgs <- c("tidyverse", "reshape2", "writexl", "httr","skimr", "janitor", "stringr")
 
 check <- sapply(pkgs, require, warn.conflicts = TRUE, character.only = TRUE)
 if (any(!check)) {
@@ -13,11 +13,11 @@ if (any(!check)) {
 
 # TRANSFORM ============================================
 # out of work ---------------------------------------------------
-stco_oow <- read.csv("V:/Sifan/Birmingham/County Cluster/source/OutOfWork_county.csv") %>%
+county_OoW <- read.csv("V:/Sifan/Birmingham/County Cluster/source/OutOfWork_county.csv") %>%
   mutate(county_code = str_pad(fips, 5, "left", "0")) %>%
   janitor::clean_names()
 
-stco_oow <- stco_oow %>%
+county_OoW <- county_OoW %>%
   mutate(young_less_educated_and_diverse_perc = young_less_educated_and_diverse,
          less_educated_prime_age_people_perc = less_educated_prime_age_people, 
          diverse_less_educated_and_eyeing_retirement_perc = diverse_less_educated_and_eyeing_retirement, 
@@ -26,76 +26,58 @@ stco_oow <- stco_oow %>%
          highly_educated_high_income_older_people_perc = highly_educated_high_income_older_people)
 
 #remove unnecessary columns 
-stco_oow$young_less_educated_and_diverse <- NULL  
-stco_oow$less_educated_prime_age_people <- NULL
-stco_oow$diverse_less_educated_and_eyeing_retirement <-NULL
-stco_oow$motivated_and_moderately_educated_younger_people<-NULL
-stco_oow$moderately_educated_older_people<-NULL
-stco_oow$highly_educated_and_engaged_younger_people<-NULL
-stco_oow$highly_educated_high_income_older_people<-NULL
-stco_oow$x_1<-NULL
-stco_oow$x<-NULL
-stco_oow$fips<-NULL
-stco_oow$cbsacode<-NULL
-stco_oow$county_name<-NULL
+county_OoW$young_less_educated_and_diverse <- NULL  
+county_OoW$less_educated_prime_age_people <- NULL
+county_OoW$diverse_less_educated_and_eyeing_retirement <-NULL
+county_OoW$motivated_and_moderately_educated_younger_people<-NULL
+county_OoW$moderately_educated_older_people<-NULL
+county_OoW$highly_educated_and_engaged_younger_people<-NULL
+county_OoW$highly_educated_high_income_older_people<-NULL
+county_OoW$x_1<-NULL
+county_OoW$x<-NULL
+county_OoW$fips<-NULL
 
 #change the percentages from "%char" to numeric .xx
 cols.num <- c("young_less_educated_and_diverse_perc","less_educated_prime_age_people_perc",             
               "diverse_less_educated_and_eyeing_retirement_perc","moderately_educated_older_people_perc",           
               "highly_educated_and_engaged_younger_people_perc","highly_educated_high_income_older_people_perc")
 
-stco_oow[cols.num] <- sapply(stco_oow,function(x) gsub("%","",as.numeric(x)))
-stco_oow[cols.num] <- sapply(stco_oow[cols.num],as.numeric)
-stco_oow[cols.num]<-stco_oow[cols.num]/100
+county_OoW[cols.num] <- sapply(county_OoW,function(x) gsub("%","",as.numeric(x)))
+
+county_OoW[cols.num] <- sapply(county_OoW[cols.num],as.numeric)
+
+county_OoW[cols.num]<-county_OoW[cols.num]/100
 
 #rename some columns
-names(stco_oow)[names(stco_oow) == 'jurisdiction'] <- 'co_name'
-names(stco_oow)[names(stco_oow) == 'county_code'] <- 'stco_code'
+names(county_OoW)[names(county_OoW) == 'jurisdiction'] <- 'county_name'
+names(county_OoW)[names(county_OoW) == 'cbsacode'] <- 'cbsa_code'
 
 #change classes
-stco_oow$stco_code<-as.character(stco_oow$stco_code)
-stco_oow$co_name<-as.character(stco_oow$co_name)
-
-#labels for metadata
-labels<-c("county name","fips code","young_less_educated_and_diverse_perc",
-          "less_educated_prime_age_people_perc",
-          "diverse_less_educated_and_eyeing_retirement_perc",
-          "moderately_educated_older_people_perc",
-          "highly_educated_and_engaged_younger_people_perc",
-          "highly_educated_high_income_older_people_perc"
-          )
-
-set_label(stco_oow)<-labels
-
-#correspondance between labels and variable names
-stco_oow_key <- get_label(stco_oow) %>%
-  data.frame() %>%
-  rename_at(vars(1), funs(paste0('labels'))) %>%
-  mutate(names = colnames(stco_oow))
-
+county_OoW$county_code<-as.character(county_OoW$county_code)
+county_OoW$county_name<-as.character(county_OoW$county_name)
+county_OoW$cbsa_code<-as.character(county_OoW$cbsa_code)
 
 # check output
 skim_with_defaults()
-skim(stco_oow)
+skim(county_OoW)
 
 # save output
 dir.create("out_of_work")
 
-save(stco_oow,file = "out_of_work/stco_oow.rda")
+save(county_OoW,file = "out_of_work/county_OoW.rda")
 
 # generate metadata county
-sink("out_of_work/stco_oow.txt")
-stco_oow_key
+sink("out_of_work/county_OoW.txt")
 skim_with(numeric = list(hist = NULL))
-skim(stco_oow)
+skim(county_OoW)
 sink()
 
-# create README county
-sink("out_of_work/README.md")
-kable(stco_oow_key)
-skim(stco_oow)%>% kable()
+# create README msa
+sink("out_of_work/county_OoW.md")
+skim(county_OoW)%>% kable()
 sink()
-
 
 # write csv to github
-write.csv(stco_oow, "out_of_work/stco_oow.csv")
+write.csv(county_OoW, "out_of_work/county_OoW.csv")
+
+
