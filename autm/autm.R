6e6ce5655eb38f6bc904f0ee7b5be4e0bab46c2a
Sifan Liu <SLiu@brookings.edu>
2019-07-03 07:14:47 +1100

    update autm

--- autm/autm.R
+++ autm/autm.R
@@ -2,7 +2,7 @@
 # Author: Sifan Liu
 # Date: Fri Aug 03 14:00:12 2018
 # SET UP ==============================================
-pkgs <- c("tidyverse", "reshape2", "writexl", "httr","skimr","sjlabelled") #sjlabelled for column labels
+pkgs <- c("tidyverse", "reshape2", "writexl", "httr", "skimr", "sjlabelled") # sjlabelled for column labels
 
 check <- sapply(pkgs, require, warn.conflicts = TRUE, character.only = TRUE)
 if (any(!check)) {
@@ -34,31 +34,31 @@ county_autm <- AUTM %>%
   mutate(stco_code = str_pad(as.character(FIPS), 5, "left", "0")) %>%
   select(-FIPS)
 
-#set variable labels
+# set variable labels
 labels <- c(
-"total license and option issues",
-"total licenses, large companies",
-"total licenses, small companoes",
-"total licenses, start-ups",
-"gross license income",
-"total investment disclosures received",
-"total start ups formed",
-"start ups in home state",
-"county FIPS code"
+  "total license and option issues",
+  "total licenses, large companies",
+  "total licenses, small companoes",
+  "total licenses, start-ups",
+  "gross license income",
+  "total investment disclosures received",
+  "total start ups formed",
+  "start ups in home state",
+  "county FIPS code"
 )
 
-set_label(county_autm)<-labels
+set_label(county_autm) <- labels
 
-#correspondance between labels and variable names
+# correspondance between labels and variable names
 county_autm_key <- get_label(county_autm) %>%
   data.frame() %>%
-  rename_at(vars(1), funs(paste0('labels'))) %>%
-  mutate(names = colnames(county_autm)) 
+  rename_at(vars(1), funs(paste0("labels"))) %>%
+  mutate(names = colnames(county_autm))
 
 
-#create directory
+# create directory
 dir.create("autm")
-save(county_autm,file = "autm/autm.rda")
+save(county_autm, file = "autm/autm.rda")
 
 
 # sink metadata into .md
@@ -69,18 +69,11 @@ skim(county_autm) %>% kable()
 sink()
 
 
-#txt file with metadata
-sink("autm/autm.txt") 
+# txt file with metadata
+sink("autm/autm.txt")
 county_autm_key
 skim(county_autm)
 sink()
 
-#write csv
-write_csv(county_autm,"autm/autm.csv")
-
-
-
-
-
-
-
+# write csv
+write_csv(county_autm, "autm/autm.csv")
