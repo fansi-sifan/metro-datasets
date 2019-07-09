1ecb0b62e30b1256382cb12b05e0194ad9e59eaf
fansi-sifan <sifan1121@gmail.com>
2019-07-09 16:24:05 -0400

    add labels to save_putout function

--- R/save_output.R
+++ R/save_output.R
@@ -1,27 +1,40 @@
-save_output <- function(df,folder, file, title, contact, source){
+save_output <- function(df,labels,folder, file, title, contact, source){
   
   # create folder
   if (!dir.exists(folder)){dir.create(folder)}
   
   # save datasets
+
   save(df, file = paste0(folder,"\\/",file,".rda"))
-  write.csv(df,paste0(folder,"\\/",file,".csv"))
+  write_csv(df,paste0(folder,"\\/",file,".csv"))
   
   # generate metadata
-  sink(paste0(folder,"\\/",file,".txt"))
+  sink(paste0(folder,"\\/",file,".txt"),append = F)
   cat("Title: ",title)
   cat("\nContact: ", contact)
-  cat("\nSource: ", source, "\n\n")
-  skim_with(numeric = list(hist = NULL), integer = list(hist = NULL))
-  skim(df)
+  cat("\nSource: ", source)
+  cat("\nLast updated: ", date(), "\n\n")
+
+  print(labels%>%kable())
+  cat("\n\n")
+
+  skimr::skim_with(numeric = list(hist = NULL), integer = list(hist = NULL))
+  skimr::skim(df)%>%kable()
+  
   sink()
   
   # create README
-  sink(paste0(folder,"\\/README.md"))
+  sink(paste0(folder,"\\/README.md"),append = F)
+  
   cat("Title: ",title)
   cat("\nContact: ", contact)
-  cat("\nSource: ", source, "\n\n")
-  skim(df)%>% kable()
+  cat("\nSource: ", source)
+  cat("\nLast updated: ", date(), "\n\n")
+  
+  print(labels%>%kable())
+  cat("\n\n")
+  
+  skimr::skim(df)%>% kable()
   sink()
   
 }
