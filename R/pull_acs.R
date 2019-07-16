f038127029ff20b47afcd2395848e5c8ff2fa493
fansi-sifan <sifan1121@gmail.com>
2019-07-16 17:53:42 -0400

    use sf to quick map

--- R/pull_acs.R
+++ R/pull_acs.R
@@ -1,21 +1,47 @@
 
-# SET UP ===================
+# SET UP =========================
 library(tidycensus)
 library(tidyverse)
 
-source("R/clean_api.R")
+source("R/clean_acs.R")
 source("R/acs_var.R")
 
-# Get data ================
 key <- Sys.getenv("CENSUS_API_KEY")
 
-# update data fields HERE ----
-geo <- "county"
-yr <- 2017
-span <- 5
-var <- earnings_edu_codes
+# update data fields HERE ========
 
-# use clean_acs to get data
-df <- clean_acs(geo, var, yr,span,key)
+geo <- "tract" # "state", "county", "tract", "metropolitan statistical area/micropolitan statistical area"
+yr <- 2017 # ACS year
+span <- 5 # 1-year or 5-year
+var <- earnings_edu_codes # or search for variables below
 
+# tmp <- load_variables(yr,paste0("acs",span),cache = TRUE)
+# view(tmp)
+# var <- 'S2301_C01_001'
 
+# optional
+st <- "AL"
+co <- "073"
+map <- TRUE # if needs corresponding geographic shapefiles
+
+# Get data =======================
+
+# use base get_acs to get data ---------------
+df <- get_acs(state = st, county = co, 
+                geography = geo, variables = var, 
+                year = yr,
+                key = key, 
+                geometry = map)
+
+# quick visualization
+df %>%
+  ggplot(aes(fill = estimate)) + 
+  geom_sf(color = NA) + 
+  coord_sf() + 
+  scale_fill_viridis_c(option = "magma") 
+
+# use clean_acs to get data with descriptive labels -------
+df <- clean_acs(state = st, county = co, 
+              geography = geo, variables = var, 
+              year = yr,
+              key = key)
