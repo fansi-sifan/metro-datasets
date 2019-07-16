f3e8b93b5d5671a2b8de5bd4d8ff5b8492f5b126
David Whyman <DWhyman@brookings.edu>
2019-07-16 09:45:18 -0400

    Housing Price to Income Ratio dataset

---
+++ housing_price/housing_price.R
@@ -0,0 +1,65 @@
+library(tidyverse)
+library(skimr)
+library(expss)
+library(sjlabelled)
+source("R/save_output.R")
+
+# SET UP ====================================
+source_dir <- "source/housing_price.csv"
+folder_name <- "housing_price"
+file_name <- "housing_price"
+
+# metadata -----------
+  dt_title <- "Housing Price to Income Ratios Across Metro Areas"
+dt_src <- "Brookings Institution analysis of 2012-16 American Community Survey data"
+dt_contact <- "Sifan Liu"
+df_notes <- "Figures include only tracts where at least 10% of homes are owner-occupied;
+ percentile columns contain % of tracts where the price-income ratio is in the given percentile range"
+ 
+# read datasets
+df <- read_csv(source_dir, skip = 5)
+
+# variable labels
+set_label(df)<-colnames(df)
+
+
+# variable names
+df <- df %>%
+  select( 
+          "cbsa_code" = "CBSA",
+          "cbsa_name" =  "Metro Name",
+          "tracts" = "# Tracts",
+          "households" = "# Households",
+          "price" = "Average price",
+          "income" = "Average income",
+          "pi" = "Average price-income rato",
+          "pi_map" = "Map category",
+          "pi_10" = "Under 10th percentile",
+          "pi_90" = "Above the 90th percentile",
+          "pi_10_90" = "Between 10th-90th percentile")
+          
+#label key
+df_labels <- create_labels(df)
+
+
+# datasets
+save_datasets(df, folder = folder_name, file = file_name)
+  
+# meta file
+save_meta(df,
+      labels = df_labels, folder = folder_name, file = file_name,
+      title = dt_title, contact = dt_contact, source = dt_src, notes = df_notes
+  )
+  
+
+
+
+
+
+
+
+
+
+
+
+
