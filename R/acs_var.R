6fdeec7e5ec96510597f5af286a48335657e46f5
David Whyman <DWhyman@brookings.edu>
2019-07-26 11:29:57 -0400

    new variable codes

--- R/acs_var.R
+++ R/acs_var.R
@@ -14,3 +14,22 @@ unemp_race_codes <- c("S2301_C04_001",map_chr(seq(12,20),function(x)paste0("S230
 ed_race_codes <- map_chr(seq(28,54),function(x)paste0("S1501_C01_0",x)) # education attainment
 earnings_edu_codes <- map_chr(seq(1,6),function(x){paste0("B20004_00",x)}) # median earnings by education attainment
 
+# Transport ------------
+zero_car_codes <- c("B08201_001","B08201_002")
+
+# Earnings-----------
+earnings_race_codes <- c("B20017A_001",
+                         "B20017B_001",
+                         "B20017C_001",
+                         "B20017D_001",
+                         "B20017E_001",
+                         "B20017F_001",
+                         "B20017G_001",
+                         "B20017H_001",
+                         "B20017I_001") # median earnings by race
+
+# Migration---------
+migration_edu_codes <- c(map_chr(seq(1,9),function(x){paste0("B07009_00",x)}),
+                         map_chr(seq(10,36),function(x){paste0("B07009_0",x)}))# migration by educational attainment
+
+
