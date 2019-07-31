04464d7e063a17860ded8e11bea98b51c2db1c6c
fansi-sifan <sifan1121@gmail.com>
2019-07-31 16:51:55 -0400

    add calculation functions

--- R/acs_var.R
+++ R/acs_var.R
@@ -1,35 +1,136 @@
-# acs variables list ==================
+# acs variables list ===============================
 
-# Basics ---------------
+# RACE ---------------
+
 pop_race_codes <- c("S2301_C01_001",map_chr(seq(12,20),function(x)paste0("S2301_C01_0",x))) # total population
+calculate_pop_race <-  function(df) {
+  df %>%
+    mutate(
+      pop_total = S2301_C01_001E/100,
+      pop_wh = S2301_C01_012E/100,
+      pop_bk = S2301_C01_013E/100)
+}
+      
 pov_race_codes <- map_chr(seq(13,21),function(x)paste0("S1701_C03_0",x))  # poverty status
-drive_codes <- c("S0802_C01_001", "S0802_C02_001") #drove to work alone
+calculate_pov_race <- function(df){
+  df %>% 
+    mutate(
+      pct_belowpoverty_wh = S1701_C03_013E/100,
+      pct_belowpoverty_bk = S1701_C03_014E/100)
+}
 
-# Labor market --------------
-lfp_race_codes <- c("S2301_C02_001",map_chr(seq(12,20),function(x)paste0("S2301_C01_0",x))) # labor force participation rate
-epop_race_codes <- c("S2301_C03_001",map_chr(seq(12,20),function(x)paste0("S2301_C01_0",x))) # employment/population ratio
-unemp_race_codes <- c("S2301_C04_001",map_chr(seq(12,20),function(x)paste0("S2301_C01_0",x))) # unemployment rate
 
-# Education ------------
-ed_race_codes <- map_chr(seq(28,54),function(x)paste0("S1501_C01_0",x)) # education attainment
-earnings_edu_codes <- map_chr(seq(1,6),function(x){paste0("B20004_00",x)}) # median earnings by education attainment
+commute_race_codes = c("S0802_C01_001", map_chr(seq(12,20),function(x)paste0("S0802_C01_0",x)), # all commuter
+                       "S0802_C02_001", map_chr(seq(12,20),function(x)paste0("S0802_C02_0",x)), # drive alone
+                       "S0802_C04_001", map_chr(seq(12,20),function(x)paste0("S0802_C04_0",x))) # public transit
 
-# Transport ------------
-zero_car_codes <- c("B08201_001","B08201_002")
+calculate_commute_race <- function(df){
+  df %>%
+    mutate(commuter_wh = S0802_C01_012E*S0802_C01_001E,
+           commuter_bk = S0802_C01_013E*S0802_C01_001E,
+           pct_drivealone_total = S0802_C02_001E/S0802_C01_001E,
+           pct_drivealone_wh = S0802_C02_012E*S0802_C02_001E/commuter_wh,
+           pct_drivealone_bk = S0802_C02_013E*S0802_C02_001E/commuter_bk,
+           pct_publictrans_total = S0802_C04_001E/S0802_C01_001E,
+           pct_publictrans_wh = S0802_C04_012E*S0802_C04_001E/commuter_wh,
+           pct_publictrans_bk = S0802_C04_013E*S0802_C04_001E/commuter_wh)
+}
 
-# Earnings-----------
-earnings_race_codes <- c("B20017A_001",
-                         "B20017B_001",
-                         "B20017C_001",
-                         "B20017D_001",
-                         "B20017E_001",
-                         "B20017F_001",
-                         "B20017G_001",
-                         "B20017H_001",
-                         "B20017I_001") # median earnings by race
+emp_race_codes <- c("S2301_C02_001",map_chr(seq(12,20),function(x)paste0("S2301_C02_0",x)), # labor force participation rate
+                    "S2301_C03_001",map_chr(seq(12,20),function(x)paste0("S2301_C03_0",x)), # employment/population ratio
+                    "S2301_C04_001",map_chr(seq(12,20),function(x)paste0("S2301_C04_0",x))  # unemployment rate
+  
+)
 
-# Migration---------
+calculate_emp_race <-  function(df) {
+  df %>%
+    mutate(
+      lfp_total = S2301_C02_001E/100,
+      lfp_wh = S2301_C02_012E/100,
+      lfp_bk = S2301_C02_013E/100,
+      epratio_total = S2301_C03_001E/100,
+      epratio_wh = S2301_C03_012E/100,
+      epratio_bk = S2301_C03_013E/100,
+      unemp_total = S2301_C04_001E/100,
+      unemp_wh = S2301_C04_012E/100,
+      unemp_bk = S2301_C04_013E/100
+    )
+}
+
+edu_race_codes <- map_chr(seq(28,54),function(x)paste0("S1501_C01_0",x)) # education attainment
+
+calculate_edu_race <-  function(df) {
+  df %>%
+    mutate(pct_hs_above_wh = S1501_C01_029E/S1501_C01_028E,
+           pct_hs_above_bk = S1501_C01_035E/S1501_C01_034E,
+           pct_ba_above_wh = S1501_C01_030E/S1501_C01_028E,
+           pct_ba_above_bk = S1501_C01_036E/S1501_C01_034E)}
+
+income_race_codes <- c("B20017_001",map_chr(LETTERS[1:9],function(x){paste0("B20017",x,"_001")})) # median earnings by race
+calculate_income_race <- function(df){
+  df %>%
+    mutate(income_total = B20017_001E,
+           income_wh = B20017A_001E,
+           income_bk = B20017B_001E
+    )
+}
+
+
+
+
+# EDU ----------------
+income_edu_codes <- map_chr(seq(1,6),function(x){paste0("B20004_00",x)}) # median earnings by education attainment
+calculate_income_edu <- function(df){
+  df %>%
+    mutate(income_total = B20004_001E,
+           income_hs = B20004_003E,
+           income_ba = B20004_005E,
+           income_prof = B20004_006E
+    )
+}
+
+
 migration_edu_codes <- c(map_chr(seq(1,9),function(x){paste0("B07009_00",x)}),
                          map_chr(seq(10,36),function(x){paste0("B07009_0",x)}))# migration by educational attainment
 
+calculate_migration <- function(df){
+  df %>%
+    mutate(frominstate_ba_above = (B07009_023E+B07009_024E),
+           frominstate_total = B07009_019E,
+           fromdiffstate_ba_above = (B07009_029E+B07009_030E),
+           fromdiffstate_total = B07009_025E,
+           fromabroad_ba_above = (B07009_035E+B07009_036E),
+           fromabroad_total = B07009_031E,
+           movein_ba_above = fromabroad_ba_above+fromdiffstate_ba_above+frominstate_ba_above,
+           movein_total = fromabroad_total+fromdiffstate_total+frominstate_total,
+           pct_movein = movein_total/B07009_001E,
+           movein_pct_ba_above = movein_ba_above/movein_total)
+}
 
+
+# OTHERS -----
+zero_car_codes <- c("B08201_001","B08201_002")
+calculate_zerocar <- function(df){
+  df %>%
+    mutate(pct_nocar = B08201_002E/B08201_001E)
+}
+
+
+# FUNCTIONS =====
+calculate_acs <- function(df){
+  df %>%
+    
+    # customized calculations
+    calculate_pop_race %>%
+    calculate_income_race %>%
+    calculate_pov_race %>%
+    calculate_edu_race %>%
+    calculate_emp_race %>%
+    calculate_commute_race %>%
+    calculate_migration %>%
+    calculate_zerocar %>%
+    calculate_income_edu %>%
+    
+    # keep only the calculated outputs
+    select(-contains("E",ignore.case = F), -contains("M", ignore.case = F))
+}
