ba2042452f8d7a94db9dca26124b1638bbbe2fe3
fansi-sifan <sifan1121@gmail.com>
2019-08-06 11:08:15 -0400

    save data update workflow in a separate file

---
+++ GETDATA/helper.R
@@ -0,0 +1,25 @@
+source("../R/pip.R")
+
+# Select and merge datasets =============================
+dfs <- objects()
+
+df_cbsa_all <- mget(dfs[grep("cbsa_",dfs)])
+df_co_all <- mget(dfs[grep("county_|co_",dfs)])
+
+cbsa_all <- merge_cbsa(lapply(df_cbsa_all, keep_latest))
+co_all <- merge_co(lapply(df_co_all, keep_latest))
+
+save(cbsa_all,file = "GETDATA/data/cbsa_all.rda")
+save(co_all,file = "GETDATA/data/co_all.rda")
+
+# column names --------------------
+list_all_cbsa <- map_depth(df_cbsa_all,names,.depth = 1)
+list_all_co <- map_depth(df_co_all,names,.depth = 1)
+
+save(list_all_cbsa,file = "GETDATA/data/list_all_cbsa.rda")
+save(list_all_co,file = "GETDATA/data/list_all_co.rda")
+
+
+# cbsa_all %>%
+#   select(c(names(cbsa_univ_rd),names(cbsa_st)))%>%
+#   filter(cbsa_code == "13820")
--- R/pip.R
+++ R/pip.R
@@ -33,34 +33,7 @@ keep_latest <- function(df){
 }
 
 
-# Select and merge datasets =============================
-dfs <- objects()
-
-df_cbsa_all <- mget(dfs[grep("cbsa_",dfs)])
-df_co_all <- mget(dfs[grep("county_|co_",dfs)])
-
-cbsa_all <- merge_cbsa(lapply(df_cbsa_all, keep_latest))
-co_all <- merge_co(lapply(df_co_all, keep_latest))
-
-# save(cbsa_all,file = "cbsa_all.rda")
-# save(co_all,file = "co_all.rda")
-
-# column names --------------------
-list_all_cbsa <- map_depth(df_cbsa_all,names,.depth = 1)
-list_all_co <- map_depth(df_co_all,names,.depth = 1)
-
-save(list_all_cbsa,file = "list_all_cbsa.rda")
-save(list_all_co,file = "list_all_co.rda")
-
-
-# cbsa_all %>%
-#   select(c(names(cbsa_univ_rd),names(cbsa_st)))%>%
-#   filter(cbsa_code == "13820")
-
-
 # find cbsa_code by short name and all counties within the CBSA ------
-
-
 find_cbsa_counties <- function(msa){
   county_cbsa_st %>%
     # metro.data::county_cbsa_st %>%
