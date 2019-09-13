2ff2d134361c58f6f856b390812e00ae95e91e8a
fansi-sifan <sifan1121@gmail.com>
2019-09-13 18:59:22 -0400

    SBO typo

--- census/SBO.R
+++ census/SBO.R
@@ -6,7 +6,6 @@ key <- Sys.getenv("CENSUS_API_KEY")
 get_SBO <- function(...){
   getCensus(name = "2012/sbo",
             vars = c( "NAICS2012","NAICS2012_TTL",
-                      "FIRM"
                       "RACE_GROUP_TTL","RACE_GROUP", 
                       "ETH_GROUP","ETH_GROUP_TTL",
                       "SEX","SEX_TTL", "GEO_ID",
@@ -15,14 +14,14 @@ get_SBO <- function(...){
             key = key)
 }
 
-df <- bind_rows (
-  get_SBO(region = "county:073", regionin  = "state:01"),
-  get_SBO(region = "county:037", regionin  = "state:47"),
-  get_SBO(region = "us:*")
-)
 
-summary(as.factor(df$RACE_GROUP))
-summary(as.factor(df$RACE_GROUP_TTL))
+df <- get_SBO(region = "county:081", regionin  = "state:26")
+  
+# df <- bind_rows (
+#   get_SBO(region = "county:031", regionin  = "state:08"),
+#   # get_SBO(region = "county:037", regionin  = "state:47"),
+#   get_SBO(region = "us:*")
+# )
 
 traded_code <- c("31-33", "51", "52", "54")
 race_code <- c("96", "30", "40")
@@ -30,7 +29,9 @@ race_code <- c("96", "30", "40")
 output <- df %>%
   filter(RACE_GROUP %in% race_code)%>%
   filter(NAICS2012 %in% traded_code)%>%
-  filter(ETH_GROUP == "001"& SEX == "001") %>%
+  filter(ETH_GROUP == "001"& SEX == "001")
+
+output %>%
   group_by(state, county, RACE_GROUP) %>%
   mutate(firmdemp = as.numeric(FIRMPDEMP))%>%
   summarise(firmdemp  = sum(firmdemp)) %>%
