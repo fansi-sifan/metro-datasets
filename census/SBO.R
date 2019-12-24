00289c737ce52840c5fcedcfbdcc7106d57ca4a0
Sifan Liu <SLiu@brookings.edu>
2019-12-25 01:43:25 +1000

    add more columns

--- census/SBO.R
+++ census/SBO.R
@@ -1,39 +1,40 @@
+# set up -----------------
 library(censusapi)
 library(tidyverse)
 
 key <- Sys.getenv("CENSUS_API_KEY")
 
-get_SBO <- function(...){
-  getCensus(name = "2012/sbo",
-            vars = c( "NAICS2012","NAICS2012_TTL",
-                      "RACE_GROUP_TTL","RACE_GROUP", 
-                      "ETH_GROUP","ETH_GROUP_TTL",
-                      "SEX","SEX_TTL", "GEO_ID",
-                      "FIRMALL", "FIRMPDEMP"),
-            ...,
-            key = key)
-}
-
-
-df <- get_SBO(region = "county:081", regionin  = "state:26")
-  
-# df <- bind_rows (
-#   get_SBO(region = "county:031", regionin  = "state:08"),
-#   # get_SBO(region = "county:037", regionin  = "state:47"),
-#   get_SBO(region = "us:*")
-# )
-
 traded_code <- c("31-33", "51", "52", "54")
 race_code <- c("96", "30", "40")
 
-output <- df %>%
-  filter(RACE_GROUP %in% race_code)%>%
-  filter(NAICS2012 %in% traded_code)%>%
-  filter(ETH_GROUP == "001"& SEX == "001")
+# func---------------------
+get_SBO <- function(...){
+  censusapi::getCensus(name = "2012/sbo",
+                       vars = c("GEO_ID","GEO_TTL", # geography
+                                "NAICS2012","NAICS2012_TTL", # industry sectors
+                                "RACE_GROUP_TTL","RACE_GROUP",  # race
+                                "ETH_GROUP","ETH_GROUP_TTL", # ethnicity
+                                "SEX","SEX_TTL", # gender
+                                "VET_GROUP", "VET_GROUP_TTL", # veteran
+                                "FIRMALL", "FIRMPDEMP", "FIRMPDEMP_S", "FIRMPDEMP_F", "FIRMPDEMP_S_F"),
+                       ...,
+                       key = key)
+}
 
-output %>%
-  group_by(state, county, RACE_GROUP) %>%
-  mutate(firmdemp = as.numeric(FIRMPDEMP))%>%
-  summarise(firmdemp  = sum(firmdemp)) %>%
-  spread(RACE_GROUP, firmdemp) %>%
-  mutate(pct_bk = `96`*0.19 - `40`)
+# test ---------------------
+# metro
+# df <- get_SBO(region = "metropolitan statistical area/micropolitan statistical area:24340")
+
+# county
+get_sbo_co <- function(stco_code) {
+  
+  
+  co_code <- paste0("county:",str_sub(stco_code, 3, 5))
+  st_code <- paste0("state:",str_sub(stco_code, 1, 2))
+  
+  get_SBO(region = co_code, regionin = st_code)
+
+  
+}
+
+
