46c1a24b4815ef78792d4af190d64f11f42e9d11
fansi-sifan <sifan1121@gmail.com>
2020-05-20 11:08:57 -0400

    monthly jobs from bls

--- R/get_bls_cpi.R
+++ R/get_bls_cpi.R
@@ -1,7 +1,7 @@
-install.packages("blscrapeR")
-# https://github.com/keberwein/blscrapeR
 
 library(blscrapeR)
+library(tidyverse)
+library(tidylog)
 
 
 area_code <- read.delim("https://download.bls.gov/pub/time.series/cw/cw.area")
@@ -15,4 +15,21 @@ area_cpi <- df %>%
   group_by(area_name, seriesID, year) %>%
   summarise(value = mean(value))
   
+# monthly jobs
+
+area_code <- read_delim("R/la_area.txt", delim = "\t")%>% 
+  filter(area_type_code == "B") %>% 
+  mutate(cbsa_code = str_sub(area_code, 5,9),
+         series = paste0("LAU",area_code,"05")) %>% 
+  left_join(metro.data::cbsa_st[c("cbsa_code", "cbsa_is.top100")], by = "cbsa_code") %>% 
+  unique() %>% 
+  filter(cbsa_is.top100 | is.na(cbsa_is.top100))
+
+area_code[c(50:100),c("series")]
+
+df <- bind_rows(bls_api(area_code[c(1:50),c("series")], startyear = 2007, endyear = 2019, registrationKey = Sys.getenv("BLS_key")),
+                bls_api(area_code[c(51:100),c("series")], startyear = 2007, endyear = 2019, registrationKey = Sys.getenv("BLS_key")),
+                bls_api(area_code[c(101:121),c("series")], startyear = 2007, endyear = 2019, registrationKey = Sys.getenv("BLS_key")))
+
+# search_ids(c("employment", "atlanta"))
 
