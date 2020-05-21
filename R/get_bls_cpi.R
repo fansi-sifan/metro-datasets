f0cc8e3ba38be3af537279a03fe6e49980e8b13d
fansi-sifan <sifan1121@gmail.com>
2020-05-21 16:26:26 -0400

    largest metros only

--- R/get_bls_cpi.R
+++ R/get_bls_cpi.R
@@ -16,20 +16,34 @@ area_cpi <- df %>%
   summarise(value = mean(value))
   
 # monthly jobs
+load("metro_monitor_2020/cbsa_metromonitor_2020.rda")
 
-area_code <- read_delim("R/la_area.txt", delim = "\t")%>% 
+cbsa_53 <- cbsa_metromonitor_2020 %>% 
+  filter(largest == 1) %>% 
+  select(cbsa_code) %>% 
+  unique()
+
+area_code <- read_delim("https://download.bls.gov/pub/time.series/la/la.area", delim = "\t")%>% 
   filter(area_type_code == "B") %>% 
   mutate(cbsa_code = str_sub(area_code, 5,9),
-         series = paste0("LAU",area_code,"05")) %>% 
-  left_join(metro.data::cbsa_st[c("cbsa_code", "cbsa_is.top100")], by = "cbsa_code") %>% 
-  unique() %>% 
-  filter(cbsa_is.top100 | is.na(cbsa_is.top100))
+         series_U = paste0("LAU",area_code,"05"),
+         series_S = paste0("LAS",area_code,"05")) %>% 
+  # left_join(metro.data::cbsa_st[c("cbsa_code", "cbsa_is.top100")], by = "cbsa_code") %>% 
+  # unique() %>% 
+  # filter(cbsa_is.top100 | is.na(cbsa_is.top100)) %>% 
+  filter(cbsa_code %in% cbsa_53$cbsa_code)
 
-area_code[c(50:100),c("series")]
 
-df <- bind_rows(bls_api(area_code[c(1:50),c("series")], startyear = 2007, endyear = 2019, registrationKey = Sys.getenv("BLS_key")),
-                bls_api(area_code[c(51:100),c("series")], startyear = 2007, endyear = 2019, registrationKey = Sys.getenv("BLS_key")),
-                bls_api(area_code[c(101:121),c("series")], startyear = 2007, endyear = 2019, registrationKey = Sys.getenv("BLS_key")))
+# df <- bind_rows(bls_api(area_code[c(1:50),c("series")], startyear = 2007, endyear = 2019, registrationKey = Sys.getenv("BLS_key")),
+#                 bls_api(area_code[c(51:100),c("series")], startyear = 2007, endyear = 2019, registrationKey = Sys.getenv("BLS_key")),
+#                 bls_api(area_code[c(101:121),c("series")], startyear = 2007, endyear = 2019, registrationKey = Sys.getenv("BLS_key")))
+
+job_monthly_cbsa53 <- bls_api(area_code$series_U, startyear = 1990, endyear = 2019, registrationKey = Sys.getenv("BLS_key")) %>% 
+  left_join(area_code[c("area_text", "cbsa_code", "series_U")], by = c("seriesID" = "series_U"))
+
+
+# save(job_monthly_cbsa53_S, file = "census/job_monthly_cbsa_53_S.rda")
+save(job_monthly_cbsa53, file = "census/job_monthly_cbsa_53.rda")
 
 # search_ids(c("employment", "atlanta"))
 
