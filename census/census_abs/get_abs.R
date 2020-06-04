516ff1919b575eb066dfb92d228afc6946162f75
fansi-sifan <sifan1121@gmail.com>
2020-06-04 17:47:36 -0400

    add employment size

--- census/census_abs/get_abs.R
+++ census/census_abs/get_abs.R
@@ -5,11 +5,12 @@ library(censusapi)
 us_abs_naics6 <- getCensus(name = "abscs",
                             vintage = 2017,
                             vars = c("GEO_ID","NAICS2017", "NAICS2017_LABEL",
-                                     # "EMPSZFI","EMPSZFI_LABEL",
+                                     "EMPSZFI","EMPSZFI_LABEL",
                                      "SEX", "SEX_LABEL","ETH_GROUP", "ETH_GROUP_LABEL","RACE_GROUP", "RACE_GROUP_LABEL",
                                      "FIRMPDEMP","RCPPDEMP", "EMP", "PAYANN"),
                             region = "us:*",
                             key = Sys.getenv("CENSUS_API_KEY"))
+save(us_abs_naics6, file = "census/census_abs/us_abs_naics6.rda")
 
 stco_abs_naics <- getCensus(name = "abscs",
           vintage = 2017,
@@ -35,7 +36,6 @@ cbsa_abs_fsize_naics <- getCensus(name = "abscs",
 # cbsa_fsize_naics_ownership <- cbsa_fsize_ownership %>% 
 #   mutate(cbsa_code = str_sub(GEO_ID, -5,-1),
 #          naics2_code = str_pad(NAICS2017,2,"left","0")) 
-save(us_abs_naics6, file = "census/census_abs/us_abs_naics6.rda")
 save(stco_abs_naics, file = "census/census_abs/stco_naics_ownership.rda")
 save(cbsa_abs_fsize_naics, file = "census/census_abs/cbsa_naics_ownership.rda")
 
